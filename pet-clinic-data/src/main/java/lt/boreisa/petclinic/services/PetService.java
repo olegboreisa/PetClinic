d5c9f89645b22ad735bc7ab0cc440232cb4be9b5
unknown <oboreisa@gmail.com>
2020-11-19 12:05:31 +0200

    BaseEntity Class and CrudService Interface

---
+++ pet-clinic-data/src/main/java/lt/boreisa/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package lt.boreisa.petclinic.services;
+
+import lt.boreisa.petclinic.model.Owner;
+import lt.boreisa.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService extends CrudService<Pet, Long>{
+
+//    Pet findById(Long id);
+//
+//    Pet save(Pet pet);
+//
+//    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/lt/boreisa/petclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package lt.boreisa.petclinic.services;
+import lt.boreisa.petclinic.model.Owner;
+import lt.boreisa.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService extends CrudService<Vet, Long> {
+
+//    Vet findById(Long id);
+//
+//    Vet save(Vet vet);
+//
+//    Set<Vet> findAll();
+//
+
+}
